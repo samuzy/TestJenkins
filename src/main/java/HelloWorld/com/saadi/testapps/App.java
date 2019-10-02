73b055b344866f0237a6f3d4981a1c0600e70181
Saadi Gebara <saadi.gebara@canada.ca>
2019-10-02 17:57:26 -0400

    Initial commit for hello world

---
+++ src/main/java/HelloWorld/com/saadi/testapps/App.java
@@ -0,0 +1,13 @@
+package HelloWorld.com.saadi.testapps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
